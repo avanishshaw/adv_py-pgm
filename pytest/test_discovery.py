098d4aa916a671e5f11ef70fab2ab4b3d00da84f
Avanish Kumar <ak2729@srmist.edu.in>
2026-02-16 16:35:53 +0530

    adding few more

---
+++ pytest/test_discovery.py
@@ -0,0 +1,23 @@
+# unit testing is type of testing done by the developers
+# what component they have developed they do the testing for it before integrating it to the other modules
+# get defects earlier
+# Pytest in python , junit and nunit - java
+# pytest is used ny developers and testers
+
+# test discovery - rules used create the pytest tests
+
+# files should start with
+# functions should start with atest
+
+# test case 1
+def test_case1():
+    print("Test case 1 is executed")
+
+def test_case2():
+    print("Test case 2 is executed")
+
+def test_case3():
+    print("Test case 3 is executed")
+
+def openBrowser():
+    print("Open Browser")
